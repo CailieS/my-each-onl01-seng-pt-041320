5c85b3be3fd1424d488de102777d6c2861ecf37d
Cailie Skelton <cskelton707@gmail.com>
2020-04-23 16:09:37 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,11 +5,13 @@ def my_each(names)
   yield (names[index])
   index += 1 
   end
-  names
-  else "Hey! Who's there?"
-  end
+  
+  
 end
   
 ["Desdemona", "Ophelia", "Juliet"].each do |name|
+  if name.start_with?("O")
+  puts "Hi, #{name}"
+end
   
 end
