ea4640fe77960990787b6aa274768c9257f82ebf
Cailie Skelton <cskelton707@gmail.com>
2020-04-23 17:10:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,10 @@
 def my_each(collection)
 i = 0
 while (i < collection.length)
-  puts collection[i].to_s + "X"
-  i += 1
-  yield collection
+  # puts collection[i].to_s + "X"
+  # i += 1
+  yield collection[i] 
+   i = i + 1
 end
 end
   
