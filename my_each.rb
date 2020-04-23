1da35dbfa95504c73f4c8033c0384f969a85cf74
Cailie Skelton <cskelton707@gmail.com>
2020-04-23 15:21:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each("names")
+  if block_given?
+  index = 1
+  while index is <= 3
+  yield (names[index])
+  index += 1 
+  end
+  names
+  else "Hey! Who's there?"
+  end
+end
+  
+["Desdemona", "Ophelia", "Juliet"].each do |name|
+  if name.start_with?("O")
+    puts "Hi, #{name}"
+  end
+
 end
